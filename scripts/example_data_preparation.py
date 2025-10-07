c2e2a8c90b6113f9c8d82a6d4ea003c522217864
golmschenk <greg@olmschenk.com>
2025-10-07 12:41:21 -0400

    Switch to polars-lts-cpu for older CPU support

--- scripts/example_data_preparation.py
+++ scripts/example_data_preparation.py
@@ -1,11 +1,11 @@
 from pathlib import Path
 
-from haplo.data_preparation import constantinos_kalapotharakos_format_file_to_sqlite
+from haplo.internal.data_conversion import constantinos_kalapotharakos_format_file_to_xarray_zarr_zip
 
 
 def example_data_preparation():
-    constantinos_kalapotharakos_format_file_to_sqlite(
-        Path('data/mcmc_vac_all_2k.dat'), Path('data/2k_parameters_and_phase_amplitudes.db'))
+    constantinos_kalapotharakos_format_file_to_xarray_zarr_zip(
+        Path('data/mcmc_vac_all_2k.dat'), Path('data/2k_parameters_and_phase_amplitudes.zarr.zip'))
 
 
 if __name__ == '__main__':
